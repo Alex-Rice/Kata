54b61effb808b4ebfa43d409ec0fe84129fff58b
Alex Rice <alexander.rice@sjc.ox.ac.uk>
2017-07-11 15:03:24 +0100

    CleanUp Unit Tests

--- GildedRose/Tests/GildedRoseTest.cs
+++ GildedRose/Tests/GildedRoseTest.cs
@@ -3,175 +3,229 @@ using NUnit.Framework;
 
 namespace GildedRose.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class GildedRoseTest
     {
-        [Test()]
-        public void NameTest()
+        [Test]
+        public void NameStaysInvariantTest()
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
                 new Item {Name = "foo", SellIn = 0, Quality = 0}
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual("foo", Items[0].Name);
         }
 
-        [Test()]
-        public void SulfurasQuality()
+        [TestCase(10)]
+        [TestCase(-10)]
+        public void SulfurasQualityStaysInvariant(int sellIn)
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -10, Quality = 10}
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = sellIn, Quality = 10}
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(10,Items[0].Quality);
-            Assert.AreEqual(10,Items[1].Quality);
         }
 
-        [Test()]
-        public void SulfurasSellIn()
+        [TestCase(10)]
+        [TestCase(-10)]
+        public void SulfurasSellInStaysInvariant(int sellIn)
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -10, Quality = 10}
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = sellIn, Quality = 10}
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
-            Assert.AreEqual(10, Items[0].SellIn);
-            Assert.AreEqual(-10, Items[1].SellIn);
+
+            // Assert
+            Assert.AreEqual(sellIn, Items[0].SellIn);
         }
 
-        [Test()]
-        public void QualityDegrade()
+        [Test]
+        public void CheckQualityDegradesByOne()
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
                 new Item {Name = "foo", SellIn = 10, Quality = 10 }
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(9, Items[0].Quality);
         }
 
-        [Test()]
-        public void SellInReduce()
+        [TestCase("foo")]
+        [TestCase("Aged Brie")]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert")]
+        public void CheckSellInDecreasesByOne(string name)
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
-                new Item {Name = "foo", SellIn = 10, Quality = 10 },
-                new Item {Name = "Aged Brie", SellIn = 10, Quality = 10 },
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10}
+                new Item {Name = name, SellIn = 10, Quality = 10 }
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(9, Items[0].SellIn);
         }
 
-        [Test()]
-        public void QualityNegativeCheck()
+        [Test]
+        public void CheckQualityIsBoundedBelowByZero()
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
                 new Item {Name = "foo", SellIn = 10, Quality = 10 }
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             for (int ii = 0; ii < 50; ii++)
             {
                 app.UpdateQuality();
             }
+
+            // Assert
             Assert.GreaterOrEqual(Items[0].Quality,0);
         }
 
-        [Test()]
-        public void QualityCapCheck()
+        [Test]
+        public void CheckQualityIsBoundedAboveByFifty()
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
                 new Item {Name = "Aged Brie", SellIn = 10, Quality = 10 },
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 50, Quality = 10}
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             for (int ii = 0; ii < 50; ii++)
             {
                 app.UpdateQuality();
             }
+
+            // Assert
             Assert.LessOrEqual(Items[0].Quality, 50);
             Assert.LessOrEqual(Items[1].Quality, 50);
         }
 
-        [Test()]
-        public void AgedBrie()
+        [Test]
+        public void AgedBrieIncreasesQualityByOneOnUpdate()
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
                 new Item {Name = "Aged Brie", SellIn = 10, Quality = 10 },
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(11,Items[0].Quality);
         }
 
-        [Test()]
-        public void TicketQuality()
+        [TestCase(50, 11)]
+        [TestCase(10,12)]
+        [TestCase(5,13)]
+        [TestCase(0,0)]
+        public void TicketQualityCorrectlyUpdatesOnValuesGiven(int sellIn, int quality)
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 50, Quality = 10},
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10},
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10},
-                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 10}
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = 10},
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
-            Assert.AreEqual(11,Items[0].Quality);
-            Assert.AreEqual(12,Items[1].Quality);
-            Assert.AreEqual(13,Items[2].Quality);
-            Assert.AreEqual(0,Items[3].Quality);
+
+            // Assert
+            Assert.AreEqual(quality,Items[0].Quality);
         }
 
-        [Test()]
-        public void ConjuredSulfurasTest()
+        [Test]
+        public void ConjuredSulfurasTestDoesNotModifySellInAndQuality()
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
                 new Item {Name = "Conjured Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10},
                 new Item {Name = "Conjured Sulfuras, Hand of Ragnaros", SellIn = -10, Quality = 10}
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(10, Items[0].Quality);
             Assert.AreEqual(10, Items[1].Quality);
         }
 
-        [Test()]
-        public void ConjuredTest()
+        [TestCase(10,8)]
+        [TestCase(-10,6)]
+        public void ConjuredItemsDecreaseQualityByCorrectAmount(int sellIn, int finalQuality)
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
-                new Item {Name = "Conjured foo", SellIn = 10, Quality = 10},
-                new Item {Name = "Conjured foo", SellIn = -10, Quality = 10}
+                new Item {Name = "Conjured foo", SellIn = sellIn, Quality = 10}
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
-            Assert.AreEqual(8,Items[0].Quality);
-            Assert.AreEqual(6,Items[1].Quality);
+
+            // Assert
+            Assert.AreEqual(finalQuality,Items[0].Quality);
         }
 
-        [Test()]
-        public void ConjuredIncreasing()
+        [Test]
+        public void ConjuredIncreasingItemsIncreaseByCorrectAmmount()
         {
+            // Arrange
             IList<Item> Items = new List<Item>
             {
                 new Item {Name = "Conjured Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10},
                 new Item {Name = "Conjured Aged Brie", SellIn = 10, Quality = 10},
             };
             GildedRose app = new GildedRose(Items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(16, Items[0].Quality);
             Assert.AreEqual(12, Items[1].Quality);
         }
