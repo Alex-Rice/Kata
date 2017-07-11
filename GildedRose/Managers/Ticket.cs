be47195b83a696b24cbc92a4cc88bbeb375a17fc
Alex Rice <alexander.rice@sjc.ox.ac.uk>
2017-07-11 13:08:21 +0100

    Ticket class added

---
+++ GildedRose/Managers/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Managers
+{
+    class Ticket : ItemManager
+    {
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
